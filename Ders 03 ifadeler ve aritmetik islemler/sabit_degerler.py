aff07ec2c849800579566c9a5a54d9d109bc5d7c
sinanurun <sinan.urun@deu.edu.tr>
2018-01-31 11:44:54 +0300

    rty
    ytu

--- Ders 03 ifadeler ve aritmetik islemler/sabit_degerler.py
+++ Ders 03 ifadeler ve aritmetik islemler/sabit_degerler.py
@@ -2,3 +2,6 @@ deger1 = int(input("Bir sayı giriniz: "))
 deger2 = int(input("Lütfen diğer sayıyı giriniz: "))
 toplam = deger1 + deger2
 print(deger1, "+", deger2, "=", toplam)
+toplam = 5
+print(toplam)
+
